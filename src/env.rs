c66b203fd1319c2d4abad28e7558a6176856b753
ryym <ryym.64@gmail.com>
2021-12-20 23:38:35 +0900

    chore: Extract initial setup code to module
    to use it in other bin

--- src/env.rs
+++ src/env.rs
@@ -1,5 +1,28 @@
 use anyhow::{Context, Result};
+use std::{fs, path::PathBuf};
 
-pub fn must_get_env(name: &str) -> Result<String> {
+#[derive(Debug)]
+pub struct EnvSetup {
+    pub work_dir: PathBuf,
+}
+
+pub fn setup_exec_env() -> Result<EnvSetup> {
+    // Initialize the logger.
+    env_logger::Builder::new()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+
+    // Create a working directory if necessary.
+    let work_dir = [&must_get_env("HOME")?, ".gharry"]
+        .iter()
+        .collect::<PathBuf>();
+    if !work_dir.as_path().exists() {
+        fs::create_dir_all(&work_dir).context("failed to create config directory")?;
+    }
+
+    Ok(EnvSetup { work_dir })
+}
+
+fn must_get_env(name: &str) -> Result<String> {
     std::env::var(name).with_context(|| format!("environment variable required: {}", name))
 }
--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,13 @@ mod slack;
 mod store;
 mod web;
 
-use crate::{config::Config, env::must_get_env};
-use anyhow::{Context, Result};
-use std::{fs, path::PathBuf, thread, time::Duration};
+use crate::config::Config;
+use anyhow::Result;
+use std::{thread, time::Duration};
 
 fn main() -> Result<()> {
-    env_logger::Builder::new()
-        .filter_level(log::LevelFilter::Debug)
-        .init();
-
-    let work_dir = [&must_get_env("HOME")?, ".gharry"]
-        .iter()
-        .collect::<PathBuf>();
-    if !work_dir.as_path().exists() {
-        fs::create_dir_all(&work_dir).context("failed to create config directory")?;
-    }
-
-    let config = Config::build_default(work_dir)?;
+    let setup = env::setup_exec_env()?;
+    let config = Config::build_default(setup.work_dir)?;
     'poll_loop: loop {
         match polling::run(&config) {
             Ok(()) => return Ok(()),
